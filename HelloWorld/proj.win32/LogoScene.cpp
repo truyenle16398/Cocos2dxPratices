ab562cfc547184e2a9498581a8c0da6080764e4c
truyenle16398 <truyenle16398@gmail.com>
2019-11-29 11:13:56 +0700

    ! Complete LogoScene

--- HelloWorld/proj.win32/LogoScene.cpp
+++ HelloWorld/proj.win32/LogoScene.cpp
@@ -20,25 +20,45 @@ bool LogoScene::init()
 
 
 	auto sprite = Sprite::create("gameloft.png");
-	if (sprite != nullptr)
+	auto sprite2 = Sprite::create("gameloft2.png");
+
+	if (sprite != nullptr && sprite2 != nullptr)
 	{
 		sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));//set vi tri cua anh
+		sprite->setScale(0.2);
+
+		sprite2->setPosition(Vec2(visibleSize.width /2 + origin.x, visibleSize.height /2 + origin.y));//set vi tri cua anh
+
 		this->addChild(sprite, 0);
+		this->addChild(sprite2, 0);
+
+		auto scaleBy = ScaleBy::create(2.0f, 3.0f);
+		sprite->runAction(scaleBy);
+		
+		/*auto rotateBy = RotateBy::create(2.0f, 360.0f);
+		sprite2->runAction(rotateBy);*/
+		auto move = MoveBy::create(2, Vec2(visibleSize.width, visibleSize.height));
+
+		auto easing = EaseBounceIn::create(move);
+		auto easing_back = easing->reverse();
+		auto seq = Sequence::create(easing,easing_back, nullptr);
+		sprite2->runAction(seq);
 	}
 
 	scheduleUpdate();
 	return true;
 }
-static auto countFrame = 0;
+static auto countFrame = 0.0f;
 void LogoScene::update(float deltaTime)
 {
 	++countFrame;
 	auto directory = Director::getInstance();
 	auto scene = HelloWorld::createScene();
-	auto scene2 = LogoScene::createScene();
-	if (countFrame == 10)
+
+	if (countFrame == 80.0f)
 	{
-		directory->replaceScene(TransitionFade::create(5, scene, Color3B(0, 0, 0)));
+		//directory->replaceScene(TransitionFade::create(5, scene, Color3B(0, 0, 0)));
+
 		//directory->replaceScene(TransitionFlipY::create(5, scene));
 		//directory->replaceScene(TransitionSlideInR::create(5, scene));
 		//directory->replaceScene(TransitionJumpZoom::create(5, scene));
